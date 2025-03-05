332b8244e6fee77355fa1e74476dddba5bd62413
Deandre Hayfa Pasha Arashna <mafiisemmm@gmail.com>
2025-03-05 11:08:53 +0700

    Rumus 3D.py

--- Rumus 3D.py
+++ Rumus 3D.py
@@ -1,5 +1,17 @@
 import math
 
-#1. fungsi luas kubus
-def LKubus (Pl, Pt, It):
-    return 2 * (Pl + Pt + It)
+#mencari volume kubus
+def VKubus (s):
+    return s * s * s
+
+#mencari volume balok
+def Vbalok (p, l, t):
+    return p * l * t
+
+#mencari volume bola
+def VBola (r):
+    return 4/3 * 3.14 * r**3
+
+#mencari volume prisma
+def VPrisma (LA, t):
+    return LA * t
